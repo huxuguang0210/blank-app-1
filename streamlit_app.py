5bb655bf8ca40a2937c8d4713b56034d20447096
huxuguang0210 <huxuguang0210@gmail.com>
2025-01-22 14:58:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,20 +9,24 @@ model = joblib.load('svm_model.joblib')
 # Streamlit 输入界面
 st.title("生育结果预测")
 
+# 创建两列布局，将手术术式选择框和提示按钮放在同一行
+col1, col2 = st.columns([4, 1])  # 两列，左列宽度较大，右列用于按钮
+
+# 手术方式
+手术方式 = st.selectbox("手术方式", [0, 1])
+
+# 手术术式选项
+手术术式 = col1.selectbox("手术术式", [1, 2, 3])
+
 # 提示按钮，点击后弹出信息
-if st.button("点击查看手术术式说明"):
+if col2.button("点击查看手术术式说明"):
     st.info("""
     1. 肿物切 = 1
     2. 一侧附件切 = 2
     3. 一侧+对侧肿物切 = 3
     """)
 
-# 输入变量
-手术方式 = st.selectbox("手术方式", [0, 1])
-
-# 手术术式选项
-手术术式 = st.selectbox("手术术式", [1, 2, 3])
-
+# 其他输入变量
 肿物破裂 = st.selectbox("肿物破裂", [0, 1])
 全面分期 = st.selectbox("全面分期", [0, 1])
 清大网 = st.selectbox("清大网", [0, 1])
